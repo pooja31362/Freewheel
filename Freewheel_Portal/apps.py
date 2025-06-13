acda337749c67b1a3de353d3baa394fe720eb57d
Nithin <nithin@gmail.com>
2025-06-13 11:46:04 +0530

    Starting the project

--- Freewheel_Portal/apps.py
+++ Freewheel_Portal/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FreeweelPortalConfig(AppConfig):
+class FreewheelPortalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Freeweel_Portal'
+    name = 'Freewheel_Portal'
--- Support_Portal/settings.py
+++ Support_Portal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Freewheel_Portal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
